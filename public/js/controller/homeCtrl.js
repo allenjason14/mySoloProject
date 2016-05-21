ccf804af4f56dc86256a2514144b2d9f9e3894af
Jason Allen <Jason Allen>
2016-05-20 21:28:47 -0600

    stuff working. now polishing

--- public/js/controller/homeCtrl.js
+++ public/js/controller/homeCtrl.js
@@ -1,4 +1,14 @@
 angular.module("wattReads").controller("homeCtrl", function($scope, storyService, mainService, user, $state){
 
   $scope.user = user.data;
+
+  $scope.readUserStory = function(){
+    storyService.readUserStory($scope.user).then(function(response){
+      console.log("hit from userList readUserStory");
+      $scope.story = response;
+    })
+  };
+
+  $scope.readUserStory();
+
 })
