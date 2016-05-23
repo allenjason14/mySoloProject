45a62c61b307d9c9df942cc25c7101f4003d4403
Jason Allen <Jason Allen>
2016-05-22 18:17:11 -0600

    Header Almost done

--- public/js/controller/mainCtrl.js
+++ public/js/controller/mainCtrl.js
@@ -1,34 +1,6 @@
  angular.module('wattReads').controller('mainCtrl', function($scope, $state, mainService){
-
-  $scope.chooseArr = [];
-  $scope.saveArr = [];
-
-  $scope.selectName = function(num) {
-    $scope.chooseArr = [];
-    $state.go('wikipage');
-    $scope.chooseArr.push($scope.story[num]);
-    console.log($scope.story[num]);
-    console.log($scope.chooseArr[0].body);
-  }
-
-
-  $scope.saveData = function(infoName, title, text){
-    var named = {
-      name: infoName,
-      body: {
-        title: title,
-        text: text
-      }
-    }
-      console.log(named);
-      $scope.saveArr.push(named);
-  }
-  //
-  //   $scope.blankArr.push(named);
-  //   $scope.infoName = '';
-  //   $scope.hello = '';
-  //   $scope.yo = '';
-  //   $scope.hey = '';
-  //   $scope.whassup = '';
-  //   $scope.peace = '';
+   $scope.toggleVisible = true;
+   $scope.toggleMenu = function {
+     $scope.toggleVisible = !$scope.toggleVisible;
+   }
 });
